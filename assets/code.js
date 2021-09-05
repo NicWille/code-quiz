f3865aa9dde85db56ca65e7fe1ae202870a29084
NicWille <nic.wille1020@yahoo.com>
2021-09-05 15:24:32 -0700

    reset quiz

--- assets/code.js
+++ assets/code.js
@@ -1,17 +1,24 @@
 let timerEl = document.getElementById('timer')
-let buttonEl = document.getElementById('btn')
-let startPageEl = document.getElementById('start-page')
-let questionPageEl = document.getElementById('question-page')
 
+let startButtonEl = document.getElementById('btn-start')
+let startPageEl = document.getElementById('start-page')
+
+let questionPageEl = document.getElementById('question-page')
 let questionEl = document.getElementById('question')
 let answer1El = document.getElementById('btn1')
 let answer2El = document.getElementById('btn2')
 let answer3El = document.getElementById('btn3')
 let answer4El = document.getElementById('btn4')
 
+let resultPageEl = document.getElementById('results-page')
+let resultEl = document.getElementById('result')
+let resetButtonEl = document.getElementById('btn-reset')
+
 let counter = 0
 
 let time = 60 
+
+let scores = []
 
 let questions = [
     {
@@ -54,11 +61,14 @@ function countdown(){
 
         timerEl.textContent = time
         time--
+        if (counter == questions.length - 1) {
+            clearInterval(timeInterval)
+        }
         if(time < -1) {
             clearInterval(timeInterval)
+            gameOver()
             time = 60
             timerEl.textContent = time
-            gameOver()
         }
     },1000)   
 }
@@ -103,7 +113,18 @@ function handleAnswers() {
 }
 
 function gameOver() {
-    console.log("game is over")
+
+    questionPageEl.classList.add("hide")
+    resultPageEl.classList.remove("hide")
+    resultEl.textContent = time
+    resetButtonEl.addEventListener("click", resetQuiz)
 }
 
-buttonEl.addEventListener("click", startQuiz)
+function resetQuiz() {
+    counter = 0
+    time = 60
+    resultPageEl.classList.add("hide")
+    startPageEl.classList.remove("hide")
+}
+
+startButtonEl.addEventListener("click", startQuiz)
